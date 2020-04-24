6d22814f6aa07474a7d16fa548c9b20820910c3c
ArtemStrelnikov <62512797+ArtemStrelnikov@users.noreply.github.com>
2020-04-24 21:08:53 +0200

    Админка Акт
    +

---
+++ git/src/about.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body>
+
+<?php 
+   include('nav.php')
+   ?>
+
+
+
+
+
+
+ 
+
+<h1>About</h1>
+<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe non nam deserunt sint ullam animi dolorem quasi perferendis ea accusantium error suscipit vero totam ex laudantium, quidem quae explicabo laborum! Alias aliquid culpa deserunt molestias. Alias temporibus quia, accusamus non quis aut voluptatum aperiam in, dicta adipisci quibusdam, numquam! Ducimus ullam praesentium, deleniti veniam atque omnis ex, libero qui laboriosam, consequatur, labore ut enim. Veritatis nesciunt voluptas doloribus tempore, perspiciatis voluptatem odit minus, sapiente officia rerum! Dicta qui eum nihil cupiditate possimus illum earum dignissimos sequi, sapiente assumenda, veniam magni fugiat accusamus praesentium reprehenderit quos neque reiciendis inventore obcaecati incidunt.</p>
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
